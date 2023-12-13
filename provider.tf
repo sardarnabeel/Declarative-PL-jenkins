19a97d95a5ad5073a9a99fb0f3fb889c676ee6c5
nabeel <nabeel@gmail.com>
2023-12-13 11:50:10 -0800

    file addedd

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  region = var.aws_region
+  profile = "nabeel"  # sso-profile-name
+}
---
+++ var.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS Region"
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+}
+
+variable "instance_name" {
+  description = "Instance Name Tag"
+}
+
+variable "stop_instance" {
+  description = "Stop EC2 Instance"
+  default     = false
+}
