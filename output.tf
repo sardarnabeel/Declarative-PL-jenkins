4ce380ebe7ab54ec4e8bd947e4a85943a07de179
nabeel <nabeel@gmail.com>
2023-12-13 18:44:51 -0800

    added

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
+//use it when conditionally create ec2 instance
+# output "instance_id" {
+#   value = aws_instance.example[0].id
+# }
+
+
 output "instance_id" {
-  value = aws_instance.example[0].id
+  value = aws_instance.example.id
 }
